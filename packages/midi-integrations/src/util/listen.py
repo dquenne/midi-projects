c37628c2830fbf9fa5253786f9ab9571283b4718
Dylan Quenneville <dquenne@gmail.com>
2022-05-25 16:16:03 -0700

    Split up more useful connection utils

--- packages/midi-integrations/src/util/listen.py
+++ packages/midi-integrations/src/util/listen.py
@@ -1,11 +1,7 @@
-from mido import get_input_names, open_input
+from mido import Message, get_input_names, get_output_names, open_input, open_output
 
 
-def log_incoming_midi_messages():
-    """
-    Very barebones utility to open a MIDI port and log messages as they come in.
-    """
-    available_midi_ports = get_input_names()
+def get_port_name_prompt(available_midi_ports):
 
     if len(available_midi_ports) > 1:
         port_names_list = "\n ".join(
@@ -16,10 +12,38 @@ def log_incoming_midi_messages():
     else:
         port_name = available_midi_ports[0]
 
+    return port_name
+
+
+def get_in_port_name_prompt():
+    return get_port_name_prompt(get_input_names())
+
+
+def get_out_port_name_prompt():
+    return get_port_name_prompt(get_output_names())
+
+
+def get_in_port():
+    return open_input(get_in_port_name_prompt())
+
+
+def get_out_port():
+    return open_output(get_out_port_name_prompt())
+
+
+def log_incoming_midi_messages():
+    """
+    Very barebones utility to open a MIDI port and log messages as they come in.
+    """
+
+    port_name = get_port_name_prompt(get_input_names())
+
     if not port_name:
         return
 
     with open_input(port_name) as in_port:
         print(f"listening on port: {port_name}...")
         for message in in_port:
+            if message.type == "clock":
+                continue
             print(message)
